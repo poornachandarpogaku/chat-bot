6a209fd949de5dcef0345e78f2210cf68c6e0bd1
poornachandarpogaku <poornachandar.pogaku@kanerika.com>
2022-08-25 13:14:15 +0530

    commit 3

---
+++ src/com/chat/Chat.java
@@ -0,0 +1,26 @@
+package com.chat;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+// write a chat program, print last 4 conversations.
+public class Chat 
+{
+	public static void main(String[] args) throws NoSuchElementException
+	{
+		LinkedList<String> list = new LinkedList<>();
+		System.out.println("lets chat:");
+		while(list.size()>=0)
+		{
+		Scanner sc = new Scanner(System.in);
+		list.add(sc.nextLine());
+	      if(list.size()>4)
+	      {
+	    	  list.removeFirst();
+	      }
+	      list.forEach(i -> System.out.println(i));  //lamda Expression
+		}
+		
+		
+	}
+}
